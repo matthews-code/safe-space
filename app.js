ca5907b26ab7c6a81ebfa42b902a989960fe0138
matthews-code <buensalidamatthew@gmail.com>
2024-08-16 00:07:15 +0800

    prettier

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const fs = require('fs');
-const path = require('path');
-const dotenv = require('dotenv');
-const express = require('express');
-const bodyParser = require('body-parser');
-const session = require('express-session');
-const store = require('connect-mongo');
-const exphbs  = require('express-handlebars');
-const mongoose = require('mongoose');
+const fs = require("fs");
+const path = require("path");
+const dotenv = require("dotenv");
+const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const store = require("connect-mongo");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
 const app = express(); //initializing express server
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,53 +17,58 @@ hostname = process.env.HOSTNAME;
 url = process.env.DB_URL;
 
 // Database connection
-const db = require('./models/db.js');
+const db = require("./models/db.js");
 db.connect();
 console.log("test");
 //Session template
-app.use(session({
-    'secret': 'ccapdev-session',
-    'resave': false,
-    'saveUninitialized': false,
+app.use(
+  session({
+    secret: "ccapdev-session",
+    resave: false,
+    saveUninitialized: false,
     cookie: { secure: !true },
-    store: store.create({mongoUrl: url})
-}));
+    store: store.create({ mongoUrl: url }),
+  })
+);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // Helper file
-const helpers = require('./helpers/helpers.js');
+const helpers = require("./helpers/helpers.js");
 
 //Templating Engine
-app.engine('.hbs', exphbs({
-    defaultLayout: 'main',
-    extname: '.hbs',
-    layoutsDir: path.join(__dirname, 'views/layouts'),
-    partialsDir: path.join(__dirname, 'views/partials'),
-    helpers: helpers
-}));
+app.engine(
+  ".hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: ".hbs",
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    partialsDir: path.join(__dirname, "views/partials"),
+    helpers: helpers,
+  })
+);
 
-app.set('view engine', '.hbs'); //using hbs as view engine
+app.set("view engine", ".hbs"); //using hbs as view engine
 
 // Routers
-const appRouter = require('./routes/router.js');
-app.use('/', appRouter);
+const appRouter = require("./routes/router.js");
+app.use(appRouter);
 
 //if route is not defined in the server
-app.use(function(req, res) {
-    res.status(404);
-    res.render('error', {
-        title: '404 Not found',
-        css:['global', 'error'],
-        status: {
-            code: "404",
-            message: "Not found"
-        },
-        sessionUser: req.session.uName 
-    });
+app.use(function (req, res) {
+  res.status(404);
+  res.render("error", {
+    title: "404 Not found",
+    css: ["global", "error"],
+    status: {
+      code: "404",
+      message: "Not found",
+    },
+    sessionUser: req.session.uName,
+  });
 });
 
-app.listen(port, hostname, function() {
-    console.log('Server running at: ');
-    console.log('http://' + hostname + ':' + port +  '/login');
-}); 
+app.listen(port, hostname, function () {
+  console.log("Server running at: ");
+  console.log("http://" + hostname + ":" + port + "/login");
+});
--- routes/router.js
+++ routes/router.js
@@ -1,73 +1,89 @@
 const express = require("express");
-const mainController = require('../controllers/mainController.js');
-const profileController = require('../controllers/profileController.js');
-const entryController = require('../controllers/entryController.js');
-const userController = require('../controllers/userController.js');
-const memoriesController = require('../controllers/memoriesController.js');
-const calendarController = require('../controllers/calendarController.js');
-const letterController = require('../controllers/letterController.js');
+const mainController = require("../controllers/mainController.js");
+const profileController = require("../controllers/profileController.js");
+const entryController = require("../controllers/entryController.js");
+const userController = require("../controllers/userController.js");
+const memoriesController = require("../controllers/memoriesController.js");
+const calendarController = require("../controllers/calendarController.js");
+const letterController = require("../controllers/letterController.js");
 
-const db = require('../models/db.js');
-const validation = require('../helpers/validation.js');
+const db = require("../models/db.js");
+const validation = require("../helpers/validation.js");
 
 const router = express();
 
 // GET
-router.get('/', mainController.getMainPage); //incase user tries to access this route
-router.get('/login', mainController.getLogin);
-router.get('/error', mainController.getError);
-router.get('/usererror', mainController.getUserError);
-router.get('/mainpage', mainController.getMainPage);
-router.get('/redirection', mainController.redirectUser);
-router.get('/settings', mainController.getSettingsPage);
-router.get('/image/:id', mainController.getPicture); //responsible for getting image in profiles
-router.get('/editaccount', mainController.geteditProfileAccount);
-router.get('/searchentry/:_id', mainController.getEntry);
-router.get('/indivletter/:_id', mainController.getLetter);
-router.get('/changeSort', mainController.changeSort);
+router.get("/", mainController.getMainPage); //incase user tries to access this route
+router.get("/login", mainController.getLogin);
+router.get("/error", mainController.getError);
+router.get("/usererror", mainController.getUserError);
+router.get("/mainpage", mainController.getMainPage);
+router.get("/redirection", mainController.redirectUser);
+router.get("/settings", mainController.getSettingsPage);
+router.get("/image/:id", mainController.getPicture); //responsible for getting image in profiles
+router.get("/editaccount", mainController.geteditProfileAccount);
+router.get("/searchentry/:_id", mainController.getEntry);
+router.get("/indivletter/:_id", mainController.getLetter);
+router.get("/changeSort", mainController.changeSort);
 
-router.get('/error_signup', profileController.getErrorSignup);
-router.get('/checksignup', profileController.checksignup); //checks username
-router.get('/getCheckEmail', profileController.checkemail); //checks email if it already exists
-router.get('/getEditEmail', profileController.editCheckEmail);
-router.get('/profile/:uName', profileController.getProfile);
-router.get('/logout', profileController.getLogout);
+router.get("/error_signup", profileController.getErrorSignup);
+router.get("/checksignup", profileController.checksignup); //checks username
+router.get("/getCheckEmail", profileController.checkemail); //checks email if it already exists
+router.get("/getEditEmail", profileController.editCheckEmail);
+router.get("/profile/:uName", profileController.getProfile);
+router.get("/logout", profileController.getLogout);
 
-router.get('/calendar', calendarController.getCalendar); //calendar update
-router.get('/getEntries', calendarController.getDesiredEntries); //calendar update
+router.get("/calendar", calendarController.getCalendar); //calendar update
+router.get("/getEntries", calendarController.getDesiredEntries); //calendar update
 
-router.get('/userprofile/:uName', userController.getUserProfile);
-router.get('/friendRequest', userController.friendRequest);
-router.get('/pendingRequest', userController.pendingRequest);
-router.get('/acceptRequest', userController.acceptRequest);
-router.get('/removeFriend', userController.unfriend);
+router.get("/userprofile/:uName", userController.getUserProfile);
+router.get("/friendRequest", userController.friendRequest);
+router.get("/pendingRequest", userController.pendingRequest);
+router.get("/acceptRequest", userController.acceptRequest);
+router.get("/removeFriend", userController.unfriend);
 
-router.get('/memories', memoriesController.getMemories); //memories feature
-router.get('/disablememories', profileController.disableMemories);
-router.get('/enablememories', profileController.enableMemories);
+router.get("/memories", memoriesController.getMemories); //memories feature
+router.get("/disablememories", profileController.disableMemories);
+router.get("/enablememories", profileController.enableMemories);
 
-router.get('/letters', letterController.getLetters);
-router.get('/lettersoutgoing', letterController.getLettersOutgoing);
+router.get("/letters", letterController.getLetters);
+router.get("/lettersoutgoing", letterController.getLettersOutgoing);
 
 //POST // Creation
-router.post('/checklogin', profileController.checklogin); //checks credentials and sends it back to ajax
-router.post('/signup', [db.upload.any(), validation.signupValidation()], profileController.signup);
-router.post('/editaccount', [db.upload.any(), validation.editAccountValidation()],profileController.editAccount);
-router.post('/login', validation.loginValidation(), profileController.login);
+router.post("/checklogin", profileController.checklogin); //checks credentials and sends it back to ajax
+router.post(
+  "/signup",
+  [db.upload.any(), validation.signupValidation()],
+  profileController.signup
+);
+router.post(
+  "/editaccount",
+  [db.upload.any(), validation.editAccountValidation()],
+  profileController.editAccount
+);
+router.post("/login", validation.loginValidation(), profileController.login);
 
-router.post('/createentry', [db.upload.any(), validation.createEntryValidation()], entryController.createEntry);
-router.post('/editentry', db.upload.any(),  entryController.editEntry);
-router.post('/deleteentry', entryController.deleteEntry);
+router.post(
+  "/createentry",
+  [db.upload.any(), validation.createEntryValidation()],
+  entryController.createEntry
+);
+router.post("/editentry", db.upload.any(), entryController.editEntry);
+router.post("/deleteentry", entryController.deleteEntry);
 
-router.post('/createletter', [db.upload.any(), validation.createLetterValidation()], letterController.createLetter);
+router.post(
+  "/createletter",
+  [db.upload.any(), validation.createLetterValidation()],
+  letterController.createLetter
+);
 
-router.get('/searchresults', mainController.getSearch);
+router.get("/searchresults", mainController.getSearch);
 
 //PATCH // Editing
 
 //PUT // Editing
 
 //DELETE
-router.get('/deleteaccount', profileController.deleteaccount);
+router.get("/deleteaccount", profileController.deleteaccount);
 
-module.exports = router; 
+module.exports = router;
